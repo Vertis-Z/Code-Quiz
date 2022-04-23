736550253d5f955ffa8fd1c5e932d1fc42502199
Vertis-Z <48894365+Vertis-Z@users.noreply.github.com>
2022-04-22 20:42:58 -0400

    added questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+var questionBank = [
+    {   
+        question: "Inside which HTML element do we put the JavaScript?",
+        selection: ["<script>", "<js>", "<javascript>", "<scripting>"],
+        answer: "<script>"
+    },
+    {   
+        question: "Where is the correct place to insert a JavaScript?",
+        selection: ["The <body> section", "The <head> section", "Both the <head> section and the <body> section are correct", "I don't know"],
+        answer: "Both the <head> section and the <body> section are correct"
+    },
+    {   
+        question: "The external JavaScript file must contain the <script> tag.", 
+        selection: ["True", "False", "Maybe?", "I dont know"],
+        answer: "False"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        selection: ["if i = 5", "if (i == 5)", "if i = 5 then", "if i == 5 then"],
+        answer: "if (i == 5)"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        selection: ["<script name='./xxx.js'>", "<script href='./xxx.js'>", "<script scr='./xxx.js'>", "<script ='./xxx.js'>"],
+        answer: "<script scr='./xxx.js'>"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        selection: ["'This is a comment", "//This is a comment", "<!--This is a comment-->", "<!**This is a comment**>"],
+        answer: "//This is a comment"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        selection: ["Math.max(x, y)", "ceil(x, y)", "top(x, y)", "Math.ceil(x, y) "],
+        answer: "Math.max(x, y)"
+    },
+    {
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        selection: ["if (i != 5)", "if (i <> 5)", "if i =! 5 then", "if i <> 5"],
+        answer: "if (i != 5)"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        selection: ["function = myFunction()", "function:myFunction()", "function myFunction()", "$Function()"],
+        answer: "function myFunction()"
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        selection: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+        answer: "Math.round(7.25)"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        selection: ["msg('Hello World');", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
+        answer: "alert('Hello World');"
+    }
+
+];
